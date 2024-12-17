02c3aae44817a1d61ecc13f6df1c8196e9265906
Arjun Nair~ <arjunnair101105@gmail.com>
2024-12-17 14:46:48 +0530

    clock with useRef done

--- WebDev/Week-10/10.1 clock using useRef/src/App.jsx
+++ WebDev/Week-10/10.1 clock using useRef/src/App.jsx
@@ -1,12 +1,31 @@
-
-import './App.css'
+import React, { useState, useRef } from "react"
 
 // a clock with a start and stop button 
 
 function App() {
+
+    const [time, setTime] = useState(0);
+    const intervalRef = useRef(null);
+    
+    const startTimer = () => {
+        if(intervalRef.current !== null) return; // already running ... dont do anything 
+
+        intervalRef.current = setInterval(()=>{
+            setTime( ( prevTime ) => prevTime + 1);
+        },1000)
+    }
+
+    const stopTimer = () => {
+        clearInterval(intervalRef.current);
+        intervalRef.current = null;
+    }
+
+
     return <div>
-        hi there
-    </div>
+        <h1>Timer: {time}</h1>
+        <button onClick={startTimer}>Start</button>
+        <button onClick={stopTimer}>Stop</button>
+        </div>
  
 }
 
