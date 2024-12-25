4b9e1d18edcc095fd820c2d1f1cd455577fe61db
Arjun Nair~ <arjunnair101105@gmail.com>
2024-12-25 13:52:07 +0530

    tailwind

---
+++ WebDev/Week-13/13.1 tailwind/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='bg-[#FEF6EE]'>hi there</div>
+    </>
+  )
+}
+
+export default App
