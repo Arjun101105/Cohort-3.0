98a9ac4cc7bbccaadd0b2c1304b9f257417e509d
Arjun Nair~ <arjunnair101105@gmail.com>
2025-01-04 23:41:06 +0530

    brainly frontend - started with button components

---
+++ WebDev/Week-15-1/brainly-frontend/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { Button } from './components/Buttons'
+
+function App() {
+
+
+  return (
+    <div>
+    <Button variant = "primary" size = "sm" text = "Add Content" startIcon = "" endIcon = "" onClick = {()=>{}}/>
+    </div>
+  )
+}
+
+export default App
---
+++ WebDev/Week-15-1/brainly-frontend/src/components/Buttons.tsx
@@ -0,0 +1,17 @@
+
+export interface ButtonProps {
+    variant: "primary" | "secondary";
+    size: "sm" | "md" | "lg";
+    text: string;
+    startIcon?:any;
+    endIcon?:any;
+    onClick:()=>void;
+}
+
+
+export const Button = (props: ButtonProps) => {
+return <button>{props.text}</button>
+    
+}
+
+<Button variant = "primary" size = "sm" text = "Add Content" startIcon = "" endIcon = "" onClick = {()=>{}}/>
