94be5f6df4871d824a40c815de6ec5fbbb9937a8
Arjun Nair~ <arjunnair101105@gmail.com>
2025-01-05 18:37:27 +0530

    buttons done, started with landing page

--- WebDev/Week-15-1/brainly-frontend/src/App.tsx
+++ WebDev/Week-15-1/brainly-frontend/src/App.tsx
@@ -1,15 +1,39 @@
-
-import './App.css'
-import { Button } from './components/Buttons'
+import "./App.css";
+import { Button } from "./components/Buttons";
+import { PlusIcon } from "./icons/Plusicon";
+import { ShareIcon } from "./icons/Shareicon";
 
 function App() {
-
-
   return (
-    <div>
-    <Button variant = "primary" size = "sm" text = "Add Content" startIcon = "" endIcon = "" onClick = {()=>{}}/>
+    <div className="flex">
+      <div className="w-1/4 p-4">
+        <h1 className="text-3xl font-bold flex"><img src="/public/brainly-icon.png" alt="logo" className="w-8 h-8"/>Second Brain</h1> 
+      </div>
+      <div className="flex-1 mt-4 p-4">
+      <div className="flex justify-between">
+        <div>
+          <h1 className="text-3xl font-bold">All Notes</h1>
+        </div>
+        <div className="flex">
+          <Button
+            startIcon={<ShareIcon size="md" />}
+            size="md"
+            variant="secondary"
+            text="Share Brain"
+            onClick={() => {}}
+          />
+          <Button
+            startIcon={<PlusIcon size="md" />}
+            size="md"
+            variant="primary"
+            text="Add Content"
+            onClick={() => {}}
+          />
+        </div>
+      </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- WebDev/Week-15-1/brainly-frontend/src/components/Buttons.tsx
+++ WebDev/Week-15-1/brainly-frontend/src/components/Buttons.tsx
@@ -1,17 +1,38 @@
+import { ReactElement } from "react";
+
+
+type ButtonVariant = "primary" | "secondary";
 
 export interface ButtonProps {
-    variant: "primary" | "secondary";
+    variant: ButtonVariant;
     size: "sm" | "md" | "lg";
     text: string;
-    startIcon?:any;
-    endIcon?:any;
+    startIcon?:ReactElement;
+    endIcon?:ReactElement;
     onClick:()=>void;
+}
+
+const variantStyles = {
+    primary: "bg-blue-500 text-white",
+    secondary: "bg-gray-200 text-gray-900",
+};
+
+const defaultStyles = {
+    rounded: "rounded-md flex"
+};
+
+const sizeStyles = {
+    sm: "px-2 py-1 text-sm",
+    md: "px-4 py-2 text-base",
+    lg: "px-6 py-3 text-lg",
 }
 
 
 export const Button = (props: ButtonProps) => {
-return <button>{props.text}</button>
-    
+    return <button className={`${variantStyles[props.variant]} ${defaultStyles.rounded} ${sizeStyles[props.size]}`}>
+        {props.startIcon ? <div className="pr-2">{props.startIcon}</div> : null}
+        {props.text}
+        {props.endIcon} 
+        </button>
 }
 
-<Button variant = "primary" size = "sm" text = "Add Content" startIcon = "" endIcon = "" onClick = {()=>{}}/>
